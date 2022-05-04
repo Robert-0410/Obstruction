f88d3a89d8701eebc7e19c80cceb625e29446edb
Robert-0410 <62630290+Robert-0410@users.noreply.github.com>
2022-05-03 21:51:11 -0700

    Commit for the work of 5/3/2022, read TODO on line 64 to continue.

--- Board.py
+++ Board.py
@@ -1,6 +1,15 @@
 # Models the game board and pieces that compose it
 
+from Tester import Player
+
+
 # TODO class: Game, represents entire game and any state that will be coupled
+class Game:
+    def __init__(self, player1: Player, player2: Player):
+        self.player1 = player1
+        self.player2 = player2
+
+# TODO class: GameState not sure if I need too
 # TODO class: Snapshot, snapshot of what the game will look like with specific move
 # TODO class: Tile, individual positions that make up the board game
 
--- Tester.py
+++ Tester.py
@@ -1,19 +1,24 @@
 # Connects Board and Solver to run Obstruction
 
 # TODO start here by taking command line arguments and set up the move sequence with single action AI outputting "Moved"
+import sys
+
 import console as console
 
 
 # Represents the participants playing against each other (Human, AI)
-class Agent:
-    def __init__(self, player: int):
+from Board import Game
+
+
+class Player:
+    def __init__(self, player: int, is_ai: bool):
         self.player = player
         self.player_id = assign_player_id(player)
+        self.is_ai = is_ai
 
 
 # assigns the character that player will use to occupy space on the game board, X is Max and O is Min
 def assign_player_id(player: int):
-    output = ''
     if player == 1:
         output = 'X'
     elif player == 2:
@@ -21,8 +26,43 @@ def assign_player_id(player: int):
     else:
         console.error("Player ID was not assigned to X or O.")
         return 'err'
+    return output
 
 
-agent1 = Agent(1)
-agent2 = Agent(2)
-agent_err = Agent(0)
+def run():
+    if len(sys.argv) == 3:
+        if sys.argv[1].isdigit():
+            ai_player_id = int(sys.argv[1])
+            search_method = sys.argv[2]
+        else:
+            ai_player_id = None
+            search_method = None
+            print("Improper command line input")
+            exit(0)
+    else:
+        print("Need arguments or improper format.")
+        exit(0)
+
+    if ai_player_id == 1 or ai_player_id == 2 and search_method == "MM" or search_method == 'AB':
+        supported = True
+    else:
+        supported = False
+
+    if not supported:
+        print("Arguments currently not supported.")
+        exit(0)
+    else:
+        print("AI Player: ", ai_player_id, ", ", "Search: ", search_method)
+        if ai_player_id == 1:
+            player1 = Player(1, True)
+            player2 = Player(2, False)
+        else:
+            player1 = Player(1, False)
+            player2 = Player(2, True)
+
+            game = Game(player1, player2)
+            # TODO continue here getting to the point of proper move sequencing between players
+
+
+# TODO if debug tool is needed call debug_mode() -> need to write still
+run()
