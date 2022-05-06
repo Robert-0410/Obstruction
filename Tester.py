284ecf86ae3508361ca35f498446c67bd107b780
Robert-0410 <62630290+Robert-0410@users.noreply.github.com>
2022-05-06 00:02:58 -0700

    Implemented more of the move sequencing, next Start on making a Board object so that I may update state based on player moves.

--- Tester.py
+++ Tester.py
@@ -19,11 +19,12 @@ class Player:
 
 # TODO class: Game, represents entire game and any state that will be coupled
 class Game:
-    def __init__(self, player1: Player, player2: Player):
+    def __init__(self, player1: Player, player2: Player, search_method):
         self.player1 = player1
         self.player2 = player2
-        self.current_board_state = start_game_state()
+        self.current_board_state = start_game_state()  # TODO, might make an object for the state of the game
         self.is_game_over = False
+        self.ai_search_method = search_method
 
 
 # assigns the character that player will use to occupy space on the game board, X is Max and O is Min
@@ -38,16 +39,37 @@ def assign_player_id(player: int):
     return output
 
 
+# Verify human input is correct TODO need to check for letters, currently breaks with input such as lkdfh
+def verify_human_input(human_input: list):
+    if len(human_input) > 2:
+        return False
+
+    for i in human_input:
+        if int(i) > 5:
+            return False
+    return True
+
+
 # Conducts player move depending on if human or AI
 def conduct_move(game: Game, player: Player):
     # human move
     if not player.is_ai:
-        print("call function to conduct human move")
-        return False
+        # Get human player input
+        raw_input = input("Input format row/column ex: 3/4: ")
+        move_coordinates = raw_input.split('/')
+        correct_input = verify_human_input(move_coordinates)
+        while not correct_input:
+            raw_input = input("Input format row/column ex: 3/4: ")
+            move_coordinates = raw_input.split('/')
+            correct_input = verify_human_input(move_coordinates)
+        # pass to function that places mark and alters the state accordingly
+        print(move_coordinates)
+        return True  # TODO temp return: needs to be a boolean that signals if the game is over or not
     else:
         print("call function to conduct AI move")
-        return True
-    # TODO needs to return if move ends game
+        # if game.search_method = MM call that algorithm
+        # else call the AB algorithm
+        return False  # TODO needs to return if move ends game
 
 
 def run():
@@ -75,16 +97,18 @@ def run():
     else:
         print("AI Player: ", ai_player_id, ", ", "Search: ", search_method)
         if ai_player_id == 1:
+            print("AI is player 1")
             player1 = Player(1, True)
             player2 = Player(2, False)
         else:
+            print("AI is player 2")
             player1 = Player(1, False)
             player2 = Player(2, True)
 
-            game = Game(player1, player2)
+            game = Game(player1, player2, search_method)
             # TODO continue here getting to the point of proper move sequencing between players
             while not game.is_game_over:
-                print("Entered while loop, line 86")
+                print("Entered while loop, line 90")
                 # ask for move, move method should return if player had a successful move to know if game should be
                 # terminated move should update game.is_game_over
                 game.is_game_over = conduct_move(game, player1)
@@ -92,4 +116,20 @@ def run():
 
 
 # TODO if debug tool is needed call debug_mode() -> need to write still
-run()
+def debug_run():
+    search_method = 'MM'
+    player1 = Player(1, True)  # False means not AI
+    player2 = Player(2, False)
+
+    game = Game(player1, player2, search_method)
+    while not game.is_game_over:
+        print("Entered while loop, line 90")
+        # ask for move, move method should return if player had a successful move to know if game should be
+        # terminated move should update game.is_game_over
+        game.is_game_over = conduct_move(game, player1)
+        game.is_game_over = conduct_move(game, player2)
+
+
+# run()
+
+debug_run()
